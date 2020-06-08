122b307fef15713834052cd06b4aef6137e94212
Mohammad Diab <Mohammad.Diab93@outlook.com>
2020-06-08 21:04:42 +0300

    Add User class.

---
+++ BlazorApp/Classes/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp
+{
+    public class User
+    {
+        static int currentId = 0;
+
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string FullName { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public User()
+        {
+            currentId++;
+            Id = currentId;
+        }
+    }
+}
