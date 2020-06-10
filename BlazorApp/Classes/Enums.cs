f57f49e44e1901f4e87e8d2328dd7ac58f84a61e
Mohammad Diab <Mohammad.Diab93@outlook.com>
2020-06-10 22:19:03 +0300

    Add Update modal --for add and edit items form general list.

---
+++ BlazorApp/Classes/Enums.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp
+{
+    public enum DialogMode
+    {
+        Add = 0,
+        Edit = 1
+    }
+
+    public enum PropertyType
+    {
+        Text = 0,
+        Number = 1,
+        Email = 2,
+        Phone = 3,
+        Date = 4
+    }
+}
---
+++ BlazorApp/Classes/Property.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp
+{
+    public class Property
+    {
+        public string Key { get; set; }
+
+        public PropertyType Type { get; set; }
+
+        public string Label { get; set; }
+
+        public string DefaultValue { get; set; }
+
+        public bool IsEditable { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public Property()
+        {
+            IsEditable = true;
+        }
+
+        public Property(string key, PropertyType type, string label)
+        {
+            Key = key;
+            Type = type;
+            Label = label;
+            IsEditable = true;
+        }
+    }
+}
