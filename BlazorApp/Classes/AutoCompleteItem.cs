0b159d66241709d0578990b3756586fd9bbf0ac9
Mohammad Diab <Mohammad.Diab93@outlook.com>
2020-06-26 20:24:00 +0300

    Add autocomplete component.

---
+++ BlazorApp/Classes/AutoCompleteItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp
+{
+    public class AutoCompleteItem
+    {
+        public string Key { get; set; }
+
+        public string Value { get; set; }
+
+        public List<string> AdditionalParameters { get; set; }
+
+        public AutoCompleteItem()
+        {
+            AdditionalParameters = new List<string>();
+        }
+
+        public AutoCompleteItem(string Key, string Value, params string[] AdditionalParameters)
+        {
+            this.Key = Key;
+            this.Value = Value;
+
+            this.AdditionalParameters = new List<string>();
+            this.AdditionalParameters.AddRange(AdditionalParameters);
+        }
+
+    }
+}
